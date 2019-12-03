e7dafed0f82a21f09f12103e7d0a43e40e62e8ea
diaojb <diaojb@esensoft.com>
2019-12-03 17:58:44 +0800

    缓存测试完成

--- src/main/java/com/tophousekeeper/system/interceptors/NormalInterceptor.java
+++ src/main/java/com/tophousekeeper/system/interceptors/NormalInterceptor.java
@@ -9,11 +9,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author NiceBin
  * @description: 常规的对Controller进行处理
- *                1.在访问时，根据Session检测User对象有没有，若再检测Cookie
+ *                1.在访问时，根据Session检测User对象有没有，若无再检测Cookie
  *                  取得Cookie中的账号，密码，是否自动登录信息
  *                  根据信息进行操作
  *                2.在Controller返回时，检测Session有没有User，如果有，添加用户信息传给前端
@@ -38,14 +39,18 @@ public class NormalInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
-        //检测登录信息
-        User user = (User)request.getSession().getAttribute(loginService.USER_OBJ);
-           if(user!=null){
-               if(modelAndView!=null){
-                   modelAndView.addObject("userName",user.getEmail());
-               }
-
-           }
+        //检测登录信息,这里的request.getSession(false)一定要有false
+        //因为此时response流已经输出完了，所以不能新建Session的，不然报错
+        HttpSession session  = request.getSession(false);
+        if(session !=null){
+            Object obj = session.getAttribute(loginService.USER_OBJ);
+            if(obj!=null){
+                User user = (User)obj;
+                if(modelAndView!=null){
+                    modelAndView.addObject("userName",user.getEmail());
+                }
+            }
+        }
     }
 
     @Override
